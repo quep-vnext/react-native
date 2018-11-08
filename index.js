d367fce8b77856ca3c58808a8d87f3815bdf4aeb
Adnet <phamque.adnetplus@gmail.com>
2018-11-08 07:19:12 +0700

    #1 Registering a Component

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+import React from 'react'
+import { Text, AppRegistry } from 'react-native'
+
+
+const App = () => {
+    return (
+        <Text>Some Text</Text>
+    );
+};
+
+
+ReactNative.AppRegistry.registerComponent('albums', () => App);
